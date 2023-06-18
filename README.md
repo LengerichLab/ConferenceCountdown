70205e38a25a09acca5fde2db30ed0c1989979c1
Ben Lengerich <blengerich@gmail.com>
2023-06-17 23:12:38 -0400

    organize conferences_db by moving conference information into conferences.csv and auto-populate the area from the csv information

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # ConferenceCountdown
 Webpage used to countdown time until conference deadlines.
-You can view the website live at [https://web.mit.edu/~blengeri/www/deadlines.shtml](https://web.mit.edu/~blengeri/www/deadlines.shtml).
 
-I work in the ML/Compbio space, so the conferences listed in [js/conferences_db.js](https://github.com/blengerich/ConferenceCountdown/blob/main/js/conferences_db.js) are focused on those themes.
+It uses the information in[conferences.csv](https://github.com/blengerich/ConferenceCountdown/blob/main/conferences.csv) to populate the countdown page.
+I work in the ML/Compbio space, so the conferences currently listed in [conferences.csv](https://github.com/blengerich/ConferenceCountdown/blob/main/conferences.csv) are focused on those themes, but the display will adapt to whatever is stored in that file.
 
-Feel free to suggest updates by [making an issue](https://github.com/blengerich/ConferenceCountdown/issues) on this repo, making a pull request with changes to [js/conferences_db.js](https://github.com/blengerich/ConferenceCountdown/blob/main/js/conferences_db.js), or even hosting your own version!
+Feel free to suggest updates by [making an issue](https://github.com/blengerich/ConferenceCountdown/issues) on this repo, making a pull request, or even hosting your own version!
+
+
+## Live Example
+You can view the website live on [my MIT homepage](https://web.mit.edu/~blengeri/www/deadlines.shtml).
--- js/conferences_db.js
+++ js/conferences_db.js
@@ -1,41 +1,3 @@
-// Database of conferences and deadlines.
-var conferences_csv = `venue,area,deadline,website,approx
-ML4H,Healthcare,2022-09-01 23:59:00 +0000,https://ml4health.github.io/2022/index.html,1
-MLHC,Healthcare,2023-04-12 17:59:00 -0500,https://www.mlforhc.org,0
-CHIL,Healthcare,2023-02-03 23:59:00,https://www.chilconference.org,1
-PSB,CompBio,2023-08-01 23:59:00 +0000, http://psb.stanford.edu,0
-ISMB,CompBio,2023-01-19 23:59:00 +0000,https://www.iscb.org/ismbeccb2023-submissions/proceedings,1
-RECOMB,CompBio,2022-10-07 17:00:00 -0400,http://recomb2023.bilkent.edu.tr/keydates_and_deadlines.html,1
-KDD,AI,2023-02-23 23:59:00 -0800,https://www.kdd.org/kdd2023,1
-AAAI,AI,2022-08-08 23:59:00 -1000,https://aaai.org/Conferences/AAAI-23/,1
-IJCAI,AI,2023-01-16 23:59:00 -1200,https://www.ijcai-23.org,1
-ICLR,ML,2022-09-21 8:00:00 -0700,http://www.iclr.cc,1
-AISTATS,ML,2022-10-06 23:59:00 +0000, http://www.aistats.org,1
-NeurIPS,ML,2023-05-11 13:00:00,http://nips.cc,0
-UAI,ML,2023-02-17 23:59:00 +0100,http://auai.org/uai2023,1
-MLSys,ML,2022-10-28 23:59:00 +0000,https://mlsys.org,1
-ICML,ML,2023-01-26 23:59:00 +0000,http://icml.cc,1
-`
-var processCSVData = function(data) {
-        ar = $.csv.toObjects(data);
-        for (let i=0; i < ar.length; i++) {
-            ar[i].deadline = moment(ar[i].deadline, "YYYY-MM-DD HH:mm:ss Z")
-            ar[i].approx = parseInt(ar[i].approx);
-        }
-        return ar;
-};
-var conferences = processCSVData(conferences_csv);
-console.log(conferences);
-
-var shouldDisplay = {
-    "ML": true,
-    "AI": true,
-    "CompBio": true,
-    "Healthcare": true,
-    "Linguistics": false
-};
-
-// HELPER FUNCTIONS
 var timeDescription = function(x) {
   return x.format("MM/DD/YYYY h:mm:ss A");
 };
@@ -53,18 +15,29 @@ var timeLeftDescription = function(t) {
   return days + " days, " + hours + "h " + minutes + "m " + seconds + "s";
 };
 
+
+var conferences = [];
+var areas = new Set();
+
+var shouldDisplay = {
+    "ML": true,
+    "AI": true,
+    "CompBio": true,
+    "Healthcare": true,
+    "Linguistics": false
+};
+
 // Display function, called every second or so
-function refreshDisplay() {
-  var d = moment();
-  $("#currtime").text("Current time: " + timeDescription(d));
+var refreshDisplay = function() {
+  var cur_moment = moment();
+  $("#currtime").text("Current time: " + timeDescription(cur_moment));
 
   // calculate and display deadlines
   for(var i=0; i<conferences.length; i++) {
     var conf = conferences[i];
     var checkBox = document.getElementById(conf.area);
     if (checkBox.checked === true) {
-      var timeLeft= conf.deadline - d;
-      console.log(conf, timeLeft);
+      var timeLeft= conf.deadline - cur_moment;
       warningString= "";
       if (conf.approx) { warningString= "based on previous year!"; }
       prefix = "";
@@ -73,7 +46,6 @@ function refreshDisplay() {
         prefix = "<a class=\"sd\" href=\"" + conf.website + "\">";
         suffix = "</a>";
       }
-
       $("#deadline" + i).html(
         prefix + "<div class=\"tld\">" + timeLeftDescription(timeLeft) + "</div>"
                + "<div class=\"vd\">" + conf.venue + "</div>"
@@ -89,48 +61,90 @@ function refreshDisplay() {
   }
 }
 
-$(document).ready(function() {
-  $("input").each(function() {
-      var mycookie = $.cookie($(this).attr('name'));
-      if (mycookie !== null) {
-        if (mycookie == 'true') {
-          $(this)[0].checked = true;
-        } else {
-          $(this)[0].checked = false;
-        }
-      }
-  });
-  $("input").change(function() {
-      $.cookie($(this).attr("name"), $(this).prop('checked'), {
-          path: '/',
-          expires: 365
-      });
-  });
-
-  for (var i=0; i<conferences.length; i++) {
-    var d = moment();
-    var conf = conferences[i];
-    if (conf.deadline - d <= 0) {
-      conf.deadline.add(1, 'year');
-      conf.approx = 1;
+var processCSVData = function(data) {
+    ar = $.csv.toObjects(data);
+    for (let i=0; i < ar.length; i++) {
+        ar[i].deadline = moment(ar[i].deadline, "YYYY-MM-DD HH:mm:ss Z")
+        ar[i].approx = parseInt(ar[i].approx);
     }
-  }
-  conferences.sort(function(a, b) {
-    return a.deadline - b.deadline;
-  });
-  // create divs for all deadlines and insert into DOM
-  for(var i=0; i<conferences.length; i++) {
-    $("<div class=dd id=deadline" + i + "></div>").appendTo("div#deadlinesdiv");
-    var divid = "#deadline" + i;
+    return ar;
+};
 
-    $(divid).hide();
-    $(divid).fadeIn(100*(i+1), function() { }); // create a nice fade in effect
-  }
+var parseDeadlines = function(conferences) {
+    var cur_moment = moment();
+    for (var i=0; i<conferences.length; i++) {
+        var conf = conferences[i];
+        if (conf.deadline - cur_moment <= 0) {
+          conf.deadline.add(1, 'year');
+          conf.approx = 1;
+        }
+    }
+    conferences.sort(function(a, b) {
+        return a.deadline - b.deadline;
+    });
+    return conferences;
+};
 
+var fetchConferences = function() {
+    fetch('conferences.csv')
+        .then(response => response.text())
+        .then(text => {
+            conferences = processCSVData(text);
+            
+            // create all area checkboxes.
+            for (var i=0; i<conferences.length; i++) {
+                var conf = conferences[i];
+                var checkBox = document.getElementById(conf.area);
+                if (checkBox !== null) {
+                  // Checkbox for area has been created
+                } else {
+                  // Checkbox for area has not been created
+                  $("<div>" + conf.area + ":<input type='checkbox' id='" + conf.area +"' name='" + conf.area + "' onclick= checked>&nbsp&nbsp&nbsp</div>").appendTo("div#checkboxesdiv");
+                }
+            }
+            
+            // set the state of the area checkboxes based on a cookie.
+            $("input").each(function() {
+              var mycookie = $.cookie($(this).attr('name'));
+              if (mycookie !== null) {
+                if (mycookie == 'true') {
+                  $(this)[0].checked = true;
+                } else {
+                  $(this)[0].checked = false;
+                }
+              } else { // turn everything on the first visit.
+                  $(this)[0].checked = true;
+              }
+            });
+            $("input").change(function() {
+              $.cookie($(this).attr("name"), $(this).prop('checked'), {
+                  path: '/',
+                  expires: 365
+              });
+            });
+            
+            conferences = parseDeadlines(conferences);
+            
+            // create divs for all deadlines and insert into DOM
+            for(var i=0; i<conferences.length; i++) {
+                $("<div class=dd id=deadline" + i + "></div>").appendTo("div#deadlinesdiv");
+                var divid = "#deadline" + i;
+                $(divid).hide();
+                $(divid).fadeIn(100*(i+1), function() { }); // create a nice fade in effect
+            }
+            
+            refreshDisplay();
+            }
+        );
+};
+
+$(document).ready(function() {
+  fetchConferences();
+  
+  setTimeout(1000); // First redraw is called in fetchConferences.
   // set up deadline timer to redraw
   setInterval(
     function(){ refreshDisplay(); },
     1000
   );
-  refreshDisplay();
 });
